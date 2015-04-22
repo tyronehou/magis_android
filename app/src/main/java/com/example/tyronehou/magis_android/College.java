384ec018330a70a78e9722cd9be94f32232f05ec
abaker-magis <baker.adrian.94@gmail.com>
2015-04-21 22:02:54 -0400

    College Page Layout

---
+++ app/src/main/java/com/example/tyronehou/magis_android/College.java
@@ -0,0 +1,82 @@
+package com.example.tyronehou.magis_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+/**
+ * Created by Adrian on 4/21/2015.
+ */
+public class College {
+
+    public class Announcements extends ActionBarActivity {
+
+        CheckBox cb1;
+        CheckBox cb2;
+        CheckBox cb3;
+        CheckBox cb4;
+        CheckBox cb5;
+        CheckBox cb6;
+        CheckBox cb7;
+        CheckBox cb8;
+
+        Button bAnnouncements;
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.college);
+
+
+            cb1 = (CheckBox) findViewById(R.id.checkBox1);
+            cb2 = (CheckBox) findViewById(R.id.checkBox2);
+            cb3 = (CheckBox) findViewById(R.id.checkBox3);
+            cb4 = (CheckBox) findViewById(R.id.checkBox4);
+            cb5 = (CheckBox) findViewById(R.id.checkBox5);
+            cb6 = (CheckBox) findViewById(R.id.checkBox6);
+            cb7 = (CheckBox) findViewById(R.id.checkBox7);
+            cb8 = (CheckBox) findViewById(R.id.checkBox8);
+
+            bAnnouncements.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    Intent announcementsIntent = new Intent(getApplicationContext(), com.example.tyronehou.magis_android.Announcements.class);
+                    startActivity(announcementsIntent);
+                }
+            });
+
+
+        }
+
+
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu_main, menu);
+            return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
