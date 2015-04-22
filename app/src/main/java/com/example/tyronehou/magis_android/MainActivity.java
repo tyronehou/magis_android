658e1538395334d2d2f5a68f33a6b3a8ffab0c3c
tyronehou <tyronehou@wireless1x-168-122-12-47.bu.edu>
2015-04-21 20:09:56 -0400

    (new) magis_android

---
+++ app/src/main/java/com/example/tyronehou/magis_android/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.tyronehou.magis_android;
+
+        import android.content.Intent;
+        import android.support.v7.app.ActionBarActivity;
+        import android.os.Bundle;
+        import android.view.Menu;
+        import android.view.MenuItem;
+        import android.view.View;
+        import android.view.View.OnClickListener;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    Button bLogIn;
+    // = (Button) findViewById(R.id.logIn);
+
+    TextView tUsername;
+    // = (TextView) findViewById(R.id.username);
+    TextView tPassword;
+    // = (TextView) findViewById(R.id.password);
+
+    EditText eUsername;
+    // = (EditText) findViewById(R.id.enterUsername);
+    EditText ePassword;
+    // = (EditText) findViewById(R.id.enterPassword);
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bLogIn = (Button) findViewById(R.id.logIn);
+        tUsername = (TextView) findViewById(R.id.username);
+        tPassword = (TextView) findViewById(R.id.password);
+        eUsername = (EditText) findViewById(R.id.enterUsername);
+        ePassword = (EditText) findViewById(R.id.enterPassword);
+
+        bLogIn.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(getApplicationContext(), Announcements.class);
+                startActivity(loginIntent);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
